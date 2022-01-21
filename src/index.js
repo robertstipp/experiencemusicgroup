958ddf470868402c60e6a5113f99f600726ce572
ehmatthes <robertstipp17@gmail.com>
2022-01-21 07:17:09 -0800

    Reduced home text font size

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import About from "./Pages/About";
 
 ReactDOM.render(
   <React.StrictMode>
